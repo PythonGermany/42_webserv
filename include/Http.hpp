ede9ca1cc6ccf5373a1e2d215b7fa8b29aa13274
PythonGermany <97847597+PythonGermany@users.noreply.github.com>
2023-09-18 04:10:00 +0200

    Moved http class definitions into cpp file

--- include/Http.hpp
+++ include/Http.hpp
@@ -7,36 +7,12 @@
 
 class Http : public AConnection {
  public:
-  Http(Address const &client, Address const &host) {
-    this->client = client;
-    this->host = host;
-    this->msgdelimiter = "\r\n\r\n";
-    this->msgsizelimit = 10000;
-    this->msgsize = 10000;
-    std::cout << "create: " << client << std::endl;
-  }
-  ~Http() { std::cout << "delete: " << client << std::endl; }
-  void OnHeadRecv(std::string msg) {
-    std::cout << "$$$$$$$$$ BEGIN HEAD $$$$$$$$$$ >";
-    std::cout << msg;
-    std::cout << "< $$$$$$$$$$ END HEAD $$$$$$$$$$$" << std::endl;
-    send("HTTP/1.1 200 OK\r\n");
-    send("Content-Length: 20\r\n");
-    send("Content-Type: text/html\r\n");
-    send("\r\n");
-    send("<h1>Hello World</h1>");
-  }
-  void OnBodyRecv(std::string msg) {
-    std::cout << "$$$$$$$$$ BEGIN BODY $$$$$$$$$$ >";
-    std::cout << msg;
-    std::cout << "< $$$$$$$$$$ END BODY $$$$$$$$$$$" << std::endl;
-    // send("Welcome! you send a BODY\n");
-  }
-  void OnCgiRecv(std::string msg) {
-    std::cout << "$$$$$$$$$ BEGIN CGI $$$$$$$$$$ >";
-    std::cout << msg;
-    std::cout << "< $$$$$$$$$$ END CGI $$$$$$$$$$$" << std::endl;
-  }
+  Http(Address const &client, Address const &host);
+  ~Http();
+
+  void OnHeadRecv(std::string msg);
+  void OnBodyRecv(std::string msg);
+  void OnCgiRecv(std::string msg);
 };
 
 #endif  // HTTP_HPP
