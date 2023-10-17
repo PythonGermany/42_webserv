4eda06c63870a3db6055bd82f05b0202292071d5
PythonGermany <97847597+PythonGermany@users.noreply.github.com>
2023-10-17 10:46:28 +0200

    Updated php dump test script

--- tests/websites/config_test/listing/cgi/dump.php
+++ tests/websites/config_test/listing/cgi/dump.php
@@ -1,7 +1,19 @@
 <?php
 header('Content-Type: text/plain');
-echo "_POST:\r\n";
-var_dump($_POST);
 echo "_SERVER:\r\n";
 var_dump($_SERVER);
+echo "_GET:\r\n";
+var_dump($_GET);
+echo "_POST:\r\n";
+var_dump($_POST);
+echo "_FILES:\r\n";
+var_dump($_FILES);
+echo "_REQUEST:\r\n";
+var_dump($_REQUEST);
+echo "_SESSION:\r\n";
+var_dump($_SESSION);
+echo "_ENV:\r\n";
+var_dump($_ENV);
+echo "_COOKIE:\r\n";
+var_dump($_COOKIE);
 ?>
