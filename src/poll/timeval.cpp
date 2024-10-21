9c7334d84845df6928cf16a5d7ff1e5a56121f6f
PythonGermany <97847597+PythonGermany@users.noreply.github.com>
2024-10-21 18:37:11 +0200

    Fix formatting issue

--- src/poll/timeval.cpp
+++ src/poll/timeval.cpp
@@ -1,14 +1,13 @@
 #include "timeval.hpp"
 
-timeval operator-(timeval const &lhs, timeval const &rhs)
-{
-	timeval result;
-    result.tv_sec = lhs.tv_sec - rhs.tv_sec;
-    result.tv_usec = lhs.tv_usec - rhs.tv_usec;
+timeval operator-(timeval const &lhs, timeval const &rhs) {
+  timeval result;
+  result.tv_sec = lhs.tv_sec - rhs.tv_sec;
+  result.tv_usec = lhs.tv_usec - rhs.tv_usec;
 
-    if (result.tv_usec < 0) {
-        result.tv_sec -= 1;
-        result.tv_usec += 1000000;
-    }
-    return result;
+  if (result.tv_usec < 0) {
+    result.tv_sec -= 1;
+    result.tv_usec += 1000000;
+  }
+  return result;
 }
